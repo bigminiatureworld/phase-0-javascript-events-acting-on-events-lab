5b98d09fbd700131bb74224a268b449e6aae620f
Adam Weaver <adamweaver90@gmail.com>
2022-09-09 16:13:43 -0400

    commit tree 3b057544be08d108c426ffd5ec8632dea7d9c5ae

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 1}px`;
+    }
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  } 
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        const rightNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(rightNumbers, 10);
+    
+        dodger.style.left = `${left + 1}px`;
+      }
+    });
